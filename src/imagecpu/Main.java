cc8876f5753078dcf8284ae6d924900430e1a4c5
Alsaev Dmitry <dima101203@gmail.com>
2026-02-13 17:43:28 +0700

    feat: add threads

--- src/imagecpu/Main.java
+++ src/imagecpu/Main.java
@@ -3,28 +3,28 @@ package imagecpu;
 public class Main {
     public static void main(String[] args) {
         String[] images = {
-//                "img.jpg"
+                "img.jpg",
                 "images/1024x768.jpg",
                 "images/1280x960.jpg",
                 "images/2048x1536.jpg",
-//                "images/pic1.png",
-//                "images/pic2.png",
-//                "images/pic3.jpg",
-//                "images/pic4.jpg",
-//                "images/pic5.jpg",
+                "images/pic1.png",
+                "images/pic2.png",
+                "images/pic3.jpg",
+                "images/pic4.jpg",
+                "images/pic5.jpg",
                 "images/5464x6830.jpg",
                 "images/8736x4896.jpg",
                 "images/9408x5376.jpg"
         };
 
         ImageAnalyzer analyzer = ImageAnalyzer.builder()
-//                .setFilter(new InversionFilter())
-                .setFilter(new BlackWhiteFilter(100))
-                .setFormatter(new ImageFormatter())
+                .setFilter(new InversionFilter())
+//                .setFilter(new BlackWhiteFilter(120))
+                .setFormatter(new ImageFormatter(16))
 //                .setOperationDilate(1)
                 .setOperationSharpness()
                 .setOutputPath("images/results")
-                .setTestRuns(1)
+                .setTestRuns(3)
                 .build();
 
         analyzer.analyze(images);
